a889de505c642c643c071af922126874618274a8
averen <avery@averen.dev>
2022-01-19 15:27:46 -0700

    refactor(api): improve deployment checks and prevent swallowing exceptions
    fixes sandra-4

--- src/main/kotlin/com/sandrabot/sandra/api/SandraAPI.kt
+++ src/main/kotlin/com/sandrabot/sandra/api/SandraAPI.kt
@@ -18,20 +18,16 @@ package com.sandrabot.sandra.api
 
 import com.sandrabot.sandra.Sandra
 import com.sandrabot.sandra.constants.Constants
-import com.sandrabot.sandra.utils.httpClient
-import com.sandrabot.sandra.utils.string
-import com.sandrabot.sandra.utils.toJson
+import com.sandrabot.sandra.utils.*
 import io.javalin.Javalin
 import io.javalin.apibuilder.ApiBuilder.*
 import io.javalin.http.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import io.ktor.http.*
 import io.ktor.util.cio.*
 import io.ktor.utils.io.*
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
@@ -43,9 +39,8 @@ import java.util.zip.ZipFile
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 import kotlin.collections.set
-import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.time.ExperimentalTime
-import kotlin.time.measureTime
+import kotlin.time.measureTimedValue
 
 /**
  * This class receives and processes calls to the Sandra API.
@@ -54,12 +49,18 @@ class SandraAPI(private val sandra: Sandra, private val port: Int) {
 
     private var deploymentAllowed = false
     private val secretKey = SecretKeySpec(sandra.credentials.githubSecret.toByteArray(), "HmacSHA256")
-    private val deployScope = CoroutineScope(EmptyCoroutineContext + Dispatchers.IO)
     private val api: Javalin = Javalin.create { config ->
         config.defaultContentType = "application/json"
         config.showJavalinBanner = false
         config.prefer405over404 = true
         if (sandra.development) config.enableDevLogging()
+        config.contextResolvers { resolvers ->
+            resolvers.ip = { ctx ->
+                // If the Forwarded header exists, use the for field to determine the original ip address
+                ctx.header("Forwarded")?.substringAfter("for=")?.substringBefore(",")?.substringBefore(";")
+                    ?: ctx.req.remoteAddr
+            }
+        }
     }
 
     init {
@@ -81,14 +82,14 @@ class SandraAPI(private val sandra: Sandra, private val port: Int) {
         api.error(405) { createError(it, it.status(), "Method not allowed") }
         api.error(500) { createError(it, it.status(), "Internal server error") }
 
-        api.exception(ForbiddenResponse::class.java) { e, context -> createError(context, e.status, e.message!!) }
-        api.exception(BadGatewayResponse::class.java) { e, context -> createError(context, e.status, e.message!!) }
-        api.exception(BadRequestResponse::class.java) { e, context -> createError(context, e.status, e.message!!) }
-        api.exception(UnauthorizedResponse::class.java) { e, context -> createError(context, e.status, e.message!!) }
-        api.exception(ServiceUnavailableResponse::class.java) { e, context -> createError(context, e.status, e.message!!) }
+        api.exception(HttpResponseException::class.java) { e, context ->
+            logger.info("Handler threw response exception while processing request", e)
+            createError(context, e.status, e.message!!)
+        }
 
     }
 
+    @Suppress("unused")
     fun allowDeployment() {
         deploymentAllowed = true
     }
@@ -125,74 +126,84 @@ class SandraAPI(private val sandra: Sandra, private val port: Int) {
 
     /* Route Handlers */
 
-    private fun status(context: Context) {
-        createResponse(context) {
-            it["ping"] = sandra.shards.averageGatewayPing
-            it["guilds"] = sandra.shards.guildCache.size()
-            it["requests"] = sandra.statistics.requestCount
-            val runtime = Runtime.getRuntime()
-            it["memory"] = (runtime.totalMemory() - runtime.freeMemory()) shr 20
-            it["uptime"] = (System.currentTimeMillis() - sandra.statistics.startTime) / 1000
-        }
+    private fun status(context: Context) = createResponse(context) {
+        it["ping"] = sandra.shards.averageGatewayPing
+        it["guilds"] = sandra.shards.guildCache.size()
+        it["requests"] = sandra.statistics.requestCount
+        val runtime = Runtime.getRuntime()
+        it["memory"] = (runtime.totalMemory() - runtime.freeMemory()) shr 20
+        it["uptime"] = (System.currentTimeMillis() - sandra.statistics.startTime) / 1000
     }
 
+    @OptIn(ExperimentalTime::class)
     private fun deploy(context: Context) {
-        // Ensure that a deployment was deliberately expected to prevent unauthorized deployments
+        // Ensure that the deployment was properly authorized
         if (!deploymentAllowed) {
             logger.warn("Deployment endpoint was called and denied by deploymentAllowed = false")
             throw ServiceUnavailableResponse()
         }
+
         // Validate that this request is from GitHub
         val mac = Mac.getInstance("HmacSHA256").apply { init(secretKey) }
         val signature = "sha256=" + mac.doFinal(context.bodyAsBytes()).joinToString("") { "%02x".format(it) }
         if ("X-Hub-Signature-256" !in context.headerMap()) throw UnauthorizedResponse("Missing signature header")
         if (context.header("X-Hub-Signature-256") != signature) throw ForbiddenResponse("Invalid signature header")
-        val runId = context.formParam("runId")?.toIntOrNull() ?: throw BadRequestResponse("Missing runId parameter")
+        val runId = context.formParam("runId")?.toLongOrNull() ?: throw BadRequestResponse("Missing runId parameter")
         logger.info("Verified request to deploy update from GitHub action $runId")
-        deployScope.launch { doDeploy(runId) }
-        createResponse(context, 202) {
-            it["message"] = "Deployment created"
-        }
-    }
 
-    @Suppress("BlockingMethodInNonBlockingContext")
-    @OptIn(ExperimentalTime::class)
-    private suspend fun doDeploy(runId: Int) {
-        val latestZip = File("latest-build.zip")
-        // Time how long it takes to download the file
-        val duration = measureTime {
-            val githubToken = sandra.credentials.githubToken
+        val latestBuildZip = File("latest-build.zip")
+        runBlocking(Dispatchers.IO) {
             // Retrieve the list of artifacts that were generated by this run
-            val actionArtifacts = httpClient.get<HttpResponse>(
-                "https://api.github.com/repos/sandrabot/sandra/actions/runs/$runId/artifacts"
-            ) { header("Authorization", "Bearer $githubToken") }
-            if (actionArtifacts.status != HttpStatusCode.OK) throw BadGatewayResponse("Failed to retrieve artifacts")
+            val actionArtifacts = try {
+                // This endpoint does not require any authorization
+                httpClient.get<HttpResponse>("https://api.github.com/repos/sandrabot/sandra/actions/runs/$runId/artifacts")
+            } catch (t: Throwable) {
+                logger.error("Failed to retrieve artifacts from GitHub", t)
+                throw BadGatewayResponse("Failed to retrieve artifacts")
+            }
+
             // Find the first artifact with the name "build-libs" which contains our binaries
-            val artifacts = Json.decodeFromString<JsonObject>(actionArtifacts.readText())["artifacts"]!!.jsonArray
-            val downloadUrl = artifacts.map { it.jsonObject }.first {
+            val artifactResponse = Json.decodeFromString<JsonObject>(actionArtifacts.readText())
+            // Throw if the total_count is null or already a zero
+            if ((artifactResponse.int("total_count") ?: 0) == 0) throw BadGatewayResponse("Job has no artifacts")
+            val libsArtifact = artifactResponse["artifacts"]!!.jsonArray.map { it.jsonObject }.find {
                 it.string("name") == "build-libs"
-            }.string("archive_download_url")!!
-            logger.info("Downloading artifacts from $downloadUrl")
-            // Download the binaries from the download url provided by GitHub
-            val download = httpClient.get<HttpResponse>(downloadUrl) { header("Authorization", "Bearer $githubToken") }
-            if (download.status != HttpStatusCode.OK) throw BadGatewayResponse("Failed to download artifacts")
-            download.content.copyAndClose(latestZip.writeChannel())
+            } ?: throw BadGatewayResponse("Missing build-libs artifact")
+            // Make sure that the artifacts are not expired, just to be safe
+            if (libsArtifact.boolean("expired") == true) throw BadRequestResponse("Artifact is expired")
+            val libsSizeBytes = libsArtifact.long("size_in_bytes") ?: throw BadGatewayResponse("Missing size_in_bytes")
+            val downloadUrl = libsArtifact.string("archive_download_url") ?: throw BadGatewayResponse("Missing archive_download_url")
+            logger.info("Downloading build-libs with size $libsSizeBytes from $downloadUrl")
+
+            // Time how long it takes to download the zip file from GitHub
+            val timedDownload = measureTimedValue {
+                val download = try {
+                    // This endpoint *does* require authorization with an actions scope
+                    httpClient.get<HttpResponse>(downloadUrl) {
+                        header("Authorization", "Bearer ${sandra.credentials.githubToken}")
+                    }
+                } catch (t: Throwable) {
+                    logger.error("Failed to download artifacts from GitHub", t)
+                    throw BadGatewayResponse("Failed to download artifacts")
+                }
+                download.content.copyAndClose(latestBuildZip.writeChannel())
+            }
+
+            // Finish processing the downloaded zip file from GitHub
+            logger.info("Finished downloading in ${timedDownload.duration} with size ${timedDownload.value} to ${latestBuildZip.absolutePath}")
+            ZipFile(latestBuildZip).use { zip ->
+                // Extract only the shadow jar and rename it to something more constant
+                val entry = zip.entries().toList().first { it.name.endsWith("-all.jar") }
+                zip.getInputStream(entry).use { File("sandra-latest.jar").outputStream().use(it::copyTo) }
+                logger.info("Selected and extracted binary ${entry.name} for installation")
+            }
+            // The zip is no longer necessary, so clean up
+            latestBuildZip.delete()
         }
-        val megaBytes = latestZip.length() shr 20
-        logger.info("Finished downloading artifact in $duration with size ${megaBytes}MB to ${latestZip.absolutePath}")
-        // Extract only the shadow jar and rename it to something constant
-        ZipFile(latestZip).use { zip ->
-            val regex = Regex("""sandra-\d\.\d\.\d(?:-SNAPSHOT)?-all\.jar""")
-            val entry = zip.entries().toList().first { it.name.matches(regex) }
-            zip.getInputStream(entry).use { File("sandra-latest.jar").outputStream().use(it::copyTo) }
-            logger.info("Selected and extracted binary ${entry.name} for installation")
-        }
-        latestZip.delete()
-        // If sandra-latest.jar exists it will replace the main jar
-        sandra.shutdown(restart = true)
+        createResponse(context) { it["message"] = "Deployment downloaded" }
     }
 
-    companion object {
+    private companion object {
         private val logger = LoggerFactory.getLogger(SandraAPI::class.java)
     }
 
