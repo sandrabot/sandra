b9cc1569c7660af55e464a0a5be7f6f1087f51b6
averen <avery@averen.dev>
2024-09-03 23:18:01 -0600

    rename config serializers.kt, it's driving me crazy

--- src/main/kotlin/com/sandrabot/sandra/config/Serializers.kt
+++ src/main/kotlin/com/sandrabot/sandra/config/Serializers.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 Avery Carroll and Logan Devecka
+ * Copyright 2017-2024 Avery Carroll and Logan Devecka
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
