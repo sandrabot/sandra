22e533fde6dc047f5ca3489365f01c4fd595500c
averen <avery@averen.dev>
2025-11-07 20:34:22 -0700

    build: update dependencies, use gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,10 @@
 
 plugins {
     application
-    kotlin("jvm") version "2.1.20"
-    kotlin("plugin.serialization") version "2.1.20"
-    id("com.github.gmazzo.buildconfig") version "5.6.2"
-    id("io.ktor.plugin") version "3.1.2"
+    kotlin("jvm") version "2.2.21"
+    kotlin("plugin.serialization") version "2.2.21"
+    id("com.github.gmazzo.buildconfig") version "6.0.6"
+    id("io.ktor.plugin") version "3.3.3"
 }
 
 group = "com.sandrabot"
@@ -39,17 +39,17 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json")
 
     implementation("club.minnced:jda-ktx:0.12.0")
-    implementation("net.dv8tion:JDA:5.3.2") {
+    implementation("net.dv8tion:JDA:5.6.1") {
         exclude(module = "opus-java")
     }
 
-    implementation("ch.qos.logback:logback-classic:1.5.18")
-    implementation("io.sentry:sentry-logback:8.7.0")
+    implementation("ch.qos.logback:logback-classic:1.5.21")
+    implementation("io.sentry:sentry-logback:8.27.1")
     implementation("net.jodah:expiringmap:0.5.11")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
     implementation("org.reflections:reflections:0.10.2")
-    implementation("redis.clients:jedis:5.2.0")
+    implementation("redis.clients:jedis:7.1.0")
 
     runtimeOnly(kotlin("scripting-jsr223"))
 }
